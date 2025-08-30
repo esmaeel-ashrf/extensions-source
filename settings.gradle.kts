9307925602ed0d83dc82fb26abadec68c1f625ce
ISMAIL ASHRF <55633275+esmaeel-ashrf@users.noreply.github.com>
2025-08-30 14:22:17 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
 rootProject.name = "extensions"
 
-include(":src:ar:mangapro")
+// include MangaPro
+include("src:ar:mangapro")
 project(":src:ar:mangapro").projectDir = file("src/ar/mangapro")
+
+// include lib-multisrc template that MangaPro depends on
+include("lib-multisrc:iken")
+project(":lib-multisrc:iken").projectDir = file("lib-multisrc/iken")
